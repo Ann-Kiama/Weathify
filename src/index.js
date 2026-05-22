65b0985f3727e457abc4a7c6082fe3af3fc5182c
Ann-Kiama <annkiama277@gmail.com>
2026-05-22 21:22:11 +0300

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 function refreshWeather(response){
     let temperatureElement =document.querySelector("#temperature");
     let temperature=response.data.temperature.current;
-     let cityElement=document.querySelector("#city");
-    
+    let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date= new Date(response.data.time);
+
+console.log(response.data)
+     timeElement.innerHTML=formatDate(date);
+     descriptionElement.innerHTML=response.data.condition.description;
+     humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+     windElement.innerHTML= `${response.data.wind.speed}km/h`;
      cityElement.innerHTML=response.data.city;
     temperatureElement.innerHTML= Math.round(temperature);
 }
+
+function formatDate(date){
+    let hours=date.getHours();
+    let minutes= date.getMinutes();
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sataurday"]
+    
+    let day= days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+    if (minutes<10){
+        minutes=`0${minutes}`
+    }
+}   
 
 function searchCity(city){
     let apiKey="281d3c04932a5df0f10bbof67859adta";
