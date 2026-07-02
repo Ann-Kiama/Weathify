fa3cb6406c60d356b82148f4ceb2a5a08023f4ce
Ann-Kiama <annkiama277@gmail.com>
2026-07-02 10:24:16 +0300

    Edited days

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ searchCity(searchInput.value);
 
 function formatDay(timestamp){
     let date=new Date(timestamp *1000);
-    let days=["Sun","Mon", "Tue","Wed","Thur","Fri","Sat"];
+    let days=["Tue","Wed","Thur","Fri","Sat"];
 
     return days[date.getDay()];
 }
